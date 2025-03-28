d734d44188a05b1096c606e816ce719a7399e17f
APIVerve Team <hello@apiverve.com>
2025-03-28 17:56:04 -0500

    Bug Fixes

--- src/apiverve_textsimilarity/apiClient.py
+++ src/apiverve_textsimilarity/apiClient.py
@@ -10,10 +10,10 @@ class TextsimilarityAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/textsimilarity'
+        self.base_url = 'https://api.apiverve.com/v1/textsimilarity'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
